0ca856b23562a390fb0442a92c040fe48bd14a7c
Viktor Titov <nulltomato@gmail.com>
2024-05-22 17:08:15 +0300

    change dependencies

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -17,3 +17,4 @@ export { SpanBarOptions, SpanBarOptionsData } from '../settings/SpanBarSettings'
 export { default as TTraceTimeline } from './TTraceTimeline';
 export { default as TNil } from './TNil';
 export { SpanLinkFunc, SpanLinkDef } from './links';
+export * from '@grafana/data'
